12784d7fd711a71ec03af23c462bbf196b47a12e
B234 <jiafeng5513@outlook.com>
2018-03-12 18:56:23 +0800

    Determined the function of CLI/Application/LibraryViewExtension/Manipulation

--- src/DynamoCLI/Program.cs
+++ src/DynamoCLI/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Dynamo.Applications;
 using Dynamo.Models;
+using System.IO;
 
 namespace DynamoCLI
 {
@@ -9,7 +10,25 @@ namespace DynamoCLI
         [STAThread]
         static internal void Main(string[] args)
         {
-
+            /*
+             * once the process is running,create/open a log file.
+             */
+            DateTime dt = DateTime.Now;
+            string filepath = "F:\\log-"+ dt.ToString("yyyy-MM-dd-HH-mm-ss") + ".txt";
+            FileStream fs = new FileStream(filepath, FileMode.Create);
+            fs.Close();
+            //if (!File.Exists(filepath))
+            //{
+            //    fs = new FileStream(filepath, FileMode.Create);
+            //}
+            //fs = File.OpenWrite(filepath);
+            
+            //string tmp = "CLI is running in " + dt.ToString("yyyy - MM - dd HH: mm: ss");
+            //byte[] data = System.Text.Encoding.Default.GetBytes(tmp);
+            ////开始写入
+            //fs.Write(data, 0, data.Length);
+            //fs.Flush();
+            //fs.Close();
             try
             {
                 var cmdLineArgs = StartupUtils.CommandLineArguments.Parse(args);
